355e26c54c4874e590195af8b3c2e9b9a3d1ddfa
Haleem <mahmudghali01@gmail.com>
2022-10-03 01:10:35 +0100

    update

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,6 +1,13 @@
-from my_app.views import PersonView
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 
-router = DefaultRouter()
-router.register(r'people', PersonView, basename='person')
-urlpatterns = router.urls
+
+from . import views
+
+
+
+urlpatterns = [
+    path('people/', views.person_list),
+    path('person/<int:pk>', views.person_detail),
+
+]
+
--- my_app/views.py
+++ my_app/views.py
@@ -1,13 +1,41 @@
+
 from django.shortcuts import render
-from rest_framework import viewsets
-from .serializers import PersonSerializer
+from rest_framework import status
 from .models import Person
-# Create your views here
+from .serializers import PersonSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+@api_view(['GET', 'POST'])
+def person_list(request):
+    if request.method == 'GET':
+        toys = Person.objects.all()
+        toys_serializer = PersonSerializer(toys, many=True)
+        return Response(toys_serializer.data)
+    elif request.method == 'POST':
+        toy_serializer = PersonSerializer(data=request.data)
+        if toy_serializer.is_valid():
+            toy_serializer.save()
+        return Response(toy_serializer.data,
+    status=status.HTTP_201_CREATED)
+    return Response(toy_serializer.errors,
+    status=status.HTTP_400_BAD_REQUEST)
 
-
-class PersonView(viewsets.ModelViewSet):
-    """
-    A viewset for viewing and editing user instances.
-    """
-    serializer_class = PersonSerializer
-    queryset = Person.objects.all()
+@api_view(['GET', 'PUT', 'DELETE'])
+def person_detail(request, pk):
+    try:
+        person = Person.objects.get(pk=pk)
+    except Person.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        person_serializer = PersonSerializer(person)
+        return Response(person_serializer.data)
+    elif request.method == 'PUT':
+        person_serializer = PersonSerializer(person_serializer, data=request.data)
+        if person_serializer.is_valid():
+            person_serializer.save()
+            return Response(person_serializer.data)
+        return Response(person_serializer.errors,
+        status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        person.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
